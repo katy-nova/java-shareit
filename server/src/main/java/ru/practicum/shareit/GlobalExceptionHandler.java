52817c6b0696b535a736aeffffbd60a94f67841e
Katy Nova <katy.novaaaa@yandex.ru>
2025-08-06 21:24:27 +0300

    + тесты

--- server/src/main/java/ru/practicum/shareit/GlobalExceptionHandler.java
+++ server/src/main/java/ru/practicum/shareit/GlobalExceptionHandler.java
@@ -1,23 +1,14 @@
 package ru.practicum.shareit;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.practicum.shareit.exception.AccessDenyException;
 import ru.practicum.shareit.exception.AlreadyExistsException;
 import ru.practicum.shareit.exception.NotFoundException;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
